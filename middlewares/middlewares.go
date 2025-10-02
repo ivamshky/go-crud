07599aa929d974c656cbc30e1b509c10123078c4
Shivam Jha <ivamshky1117@gmail.com>
2025-10-03 01:26:16 +0530

    logging and basic auth middlewares

---
+++ middlewares/middlewares.go
@@ -0,0 +1,39 @@
+package middlewares
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+const USER = "admin"
+const PASSWORD = "admin"
+
+func RequireAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, pwd, ok := r.BasicAuth()
+		if !ok || USER != user || PASSWORD != pwd {
+			http.Error(w, "401 Unauthorized", 401)
+			slog.Info("Basic Auth failed: ", "ok", ok, "user", user, "pwd", pwd)
+			return
+		}
+		slog.Info("User authenticated", "user", user)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Request Received.. ")
+		var (
+			contentType = r.Header.Get("Content-Type")
+			ip          = r.RemoteAddr
+			method      = r.Method
+			proto       = r.Proto
+			headers     = r.Header
+		)
+		userAttrs := slog.Group("user", "ip", ip)
+		requestAttrs := slog.Group("request", "method", method, "content-type", contentType, "protocol", proto, "headers", headers)
+		slog.Info("Request Data: ", userAttrs, requestAttrs)
+		next.ServeHTTP(w, r)
+	})
+}
--- server.go
+++ server.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/ivamshky/go-crud/middlewares"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	mux.Handle("/user", middlewares.RequireAuth(http.HandlerFunc(handleUser)))
+	log.Print("Listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", middlewares.LogRequest(mux)))
+}
+
+func handleUser(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "GET":
+		fmt.Fprint(writer, "GET /user called")
+		break
+	case "POST":
+		fmt.Fprint(writer, "POST /user called")
+		break
+	default:
+		panic("Unsupported method")
+	}
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
